6d6af7c042100c7b91c7e5bde668f3053fa8bc37
Hee Hwang <heehcs@gmail.com>
2020-08-10 10:43:54 -0400

    Set up obj, mtl, and png. Fix Perspective.

--- pathtracer-plus-plus/main.cpp
+++ pathtracer-plus-plus/main.cpp
@@ -31,8 +31,18 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
+// const std::string MODEL_PATH = "models/viking_room.obj";
+// const std::string TEXTURE_PATH = "textures/viking_room.png";
+//const std::string MODEL_PATH = "models/rungholt.obj";
+//const std::string TEXTURE_PATH = "textures/rungholt-RGB.png";
+const std::string MODEL_PATH = "models/vokselia_spawn.obj";
+const std::string TEXTURE_PATH = "textures/vokselia_spawn.png";
+//const std::string MODEL_PATH = "models/lost_empire.obj";
+//const std::string TEXTURE_PATH = "textures/lost_empire-RGBA.png";
+
+const std::string MODEL_DIR = "models/";
+
+
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
@@ -1148,7 +1158,7 @@ class HelloTriangleApplication {
     std::vector<tinyobj::material_t> materials;
     std::string warn, err;
 
-    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str())) {
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str(), MODEL_DIR.c_str())) {
       throw std::runtime_error(warn + err);
     }
 
@@ -1321,6 +1331,7 @@ class HelloTriangleApplication {
 
     vkBindBufferMemory(device, buffer, bufferMemory, 0);
   }
+	
 
   VkCommandBuffer beginSingleTimeCommands() {
     VkCommandBufferAllocateInfo allocInfo{};
@@ -1464,11 +1475,18 @@ class HelloTriangleApplication {
     float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
     UniformBufferObject ubo{};
+    ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(270.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     //ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    //ubo.model = glm::mat4(1.0f);
+    ubo.view = glm::lookAt(glm::vec3(0.5f, 0.0f, -3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f, 10.0f);
     ubo.proj[1][1] *= -1;
+
+
+
+
+
+
 
     void* data;
     vkMapMemory(device, uniformBuffersMemory[currentImage], 0, sizeof(ubo), 0, &data);
